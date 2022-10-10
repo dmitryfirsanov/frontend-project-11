31618d354337f3ebdc6ff0e781d033ace8c50cdf
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-10-11 01:21:57 +0300

    add localization, dictionary

---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  ru: {
+    translation: {
+      validation: {
+        errors: {
+          errorURL: 'Ссылка должна быть валидным URL',
+          errorUniqRSSUrl: 'RSS уже существует',
+          errorRequared: 'Не должно быть пустым',
+        },
+        isValid: 'RSS загружается',
+      },
+    },
+  },
+};
