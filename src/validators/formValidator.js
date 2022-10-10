b72a73366218cad677a8bfdee644fcca23205423
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-10-11 01:23:50 +0300

    rewrite input validation

---
+++ src/validators/formValidator.js
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+
+const validateForm = (i18next, content, listOfFeeds) => {
+  yup.setLocale({
+    mixed: { 
+      notOneOf: i18next.t('validation.errors.errorUniqRSSUrl'),
+    },
+    string: {
+      url: i18next.t('validation.errors.errorURL'),
+      min: i18next.t('validation.errors.errorRequared'),
+    },
+  });
+
+  const schema = yup.string().url().min(1).notOneOf(listOfFeeds);
+
+  return schema.validate(content);
+};
+
+export default validateForm;
