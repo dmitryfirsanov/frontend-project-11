606e13b4fa59a4d46137987d6b2055ba48179cce
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-10-17 23:26:44 +0300

    add parser rss

---
+++ src/parsers/parserRss.js
@@ -0,0 +1,25 @@
+const parserRSS = (response) => {
+  try {
+    const parser = new DOMParser();
+    const data = parser.parseFromString(response.data.contents, 'text/xml');
+    const feed = {
+      title: data.querySelector('channel title').textContent,
+      description: data.querySelector('channel description').textContent,
+      topics: Array.from(data.querySelectorAll('item')).map((item) => {
+        const top = {
+          title: item.querySelector('title').textContent,
+          link: item.querySelector('link').textContent,
+          description: item.querySelector('description').textContent,
+        };
+        
+        return top;
+      }),
+    };
+
+    return feed;
+  } catch {
+    throw new Error('parsing error');
+  }
+};
+
+export default parserRSS;
