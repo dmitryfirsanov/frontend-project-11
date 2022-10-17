7c3f2d22f74703e8d1564e6fb91f6cc9837da6c8
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-10-17 23:25:25 +0300

    add loadRss(not completely)

---
+++ src/handlers/loadRss.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { uniqueId } from 'lodash';
+import parserRss from '../parsers/parserRss';
+
+const loadRss = (url, state) => {
+  const id = uniqueId();
+  const proxy = new URL(`https://allorigins.hexlet.app/get?url=${url}`);
+  proxy.searchParams.set('disableCache', true);
+
+  axios.get(proxy.toString())
+    .catch(() => {
+      state.feedback = state.i18n.t('loading.errors.errorNetWork');
+      throw new Error();
+    })
+    .then((response) => parserRss(response))
+    .catch(({ message }) => {
+      if (message === 'errorParsing') state.feedback = state.i18n.t('loading.errors.errorResource');
+      throw new Error();
+    })
+    .then((parsedRss) => {
+      // продолжение следует
+    });
+};
+
+// дописать событие loadRss, написать watcher для этого события, и для блоканья кнопки, подключить в событие btnAddRssURL ⌨️
+// нужен ли новый рендер 🤔
+export default loadRss;
