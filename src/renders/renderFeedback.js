33e4c44f4c88e57a9dc825f35d2228e2a2237410
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-10-17 23:28:10 +0300

    rename

---
+++ src/renders/renderFeedback.js
@@ -0,0 +1,20 @@
+export default (isValid, message) => {
+  const input = document.querySelector('#url-input');
+  const feedback = document.querySelector('.feedback');
+
+  feedback.textContent = message;
+
+  if (feedback.classList.contains('text-danger')) feedback.classList.remove('text-danger');
+  else feedback.classList.remove('text-succsess');
+
+  if (isValid) {
+    if (input.classList.contains('is-invalid')) input.classList.remove('is-invalid');
+    feedback.classList.add('text-success');
+    input.value = '';
+  } else {
+    input.classList.add('is-invalid');
+    feedback.classList.add('text-danger');
+  }
+
+  input.focus();
+};
