029477ac88894eb6c2c687e3335a99d27163f7e1
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-10-19 00:28:54 +0300

    add render for rss

--- src/view/watchers.js
+++ src/view/watchers.js
@@ -1,9 +1,15 @@
 import onChange from 'on-change';
 import renderFeedback from '../renders/renderFeedback.js';
+import renderRssContent from '../renders/renderRssContent.js';
 
 export default (state) => {
-  const wathcer = onChange(state, (path, isValid) => {
-    renderFeedback(isValid, state.feedback);
+  const wathcer = onChange(state, (path, value) => {
+    if (value === null) return;
+    renderFeedback(value, state.feedback);
+    if (path === 'isLoaded' && value) {
+      renderRssContent(state);
+    }
+    wathcer[path] = null;
   });
 
   return wathcer;
