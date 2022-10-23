cde54f75766744ee149169151b6a2372c626f4b4
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-10-23 19:53:21 +0300

    add render for new topics

--- src/renders/renderRssContent.js
+++ src/renders/renderRssContent.js
@@ -41,26 +41,25 @@ const buildBlockOfPosts = (state) => {
   const blockContent = document.createElement('ul');
   blockContent.classList.add('list-group', 'border-0', 'rounded-0');
 
-  state.feeds.forEach(({ topics }) => {
-    topics.forEach((topic) => {
-      const id = uniqueId();
-      const post = document.createElement('li');
-      post.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+  state.topics.forEach((topic) => {
+    const id = uniqueId();
+    const post = document.createElement('li');
+    post.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
 
-      const link = document.createElement('a');
-      link.classList.add('fw-bold');
-      link.setAttribute('href', topic.link);
-      link.setAttribute('data-id', `${id}`);
-      link.setAttribute('target', '_blank');
-      link.setAttribute('rel', 'noopener noreferrer');
-      link.textContent = `${topic.title}`;
+    const link = document.createElement('a');
+    link.classList.add('fw-bold');
+    link.setAttribute('href', topic.link);
+    link.setAttribute('data-id', `${id}`);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer');
+    link.textContent = `${topic.title}`;
 
-      post.innerHTML = `<button type="button" class="btn btn-outline-primary btn-sm" data-id="${id}" data-bs-toggle="modal" data-bs-target="#modal">${state.i18next.t('content.view')}</button>`;
+    post.innerHTML = `<button type="button" class="btn btn-outline-primary btn-sm" data-id="${id}" data-bs-toggle="modal" data-bs-target="#modal">${state.i18next.t('content.view')}</button>`;
 
-      post.prepend(link);
-      blockContent.append(post);
-    });
+    post.prepend(link);
+    blockContent.append(post);
   });
+
   block.append(blockName, blockContent);
 
   return block;
