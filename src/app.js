771c320960e7823d3c4edef714d19815bbf47bb3
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-10-25 00:19:06 +0300

    remove watcher import

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import btnAddRssURL from './handlers/btnAddRssURL.js';
-import { watcherRss, watcherUpdateRss } from './view/watchers';
 
 const app = (state) => {
-  btnAddRssURL(watcherRss(state), watcherUpdateRss(state), state);
+  btnAddRssURL(state);
 };
 
 export default app;
