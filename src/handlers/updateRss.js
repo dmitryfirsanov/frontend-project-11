f8a3319bc4a7b23d6792c47ba8064daad62dbe08
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-10-24 17:13:43 +0300

    add setTimeout, not finished

--- src/handlers/updateRss.js
+++ src/handlers/updateRss.js
@@ -4,32 +4,36 @@ import { differenceWith, isEqual, isEmpty } from 'lodash';
 import parserRSS from '../parsers/parserRss.js';
 
 const updateRss = (watcherUpdateRss, state) => {
-  const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
+  setTimeout(() => {
+    const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
 
-  // eslint-disable-next-line arrow-body-style
-  const promises = state.resources.map((resource) => {
-    return axios.get(`${proxy}${resource}`)
-      .catch(() => {
-        state.feedback = state.i18next.t('loading.errors.errorNetWork');
-        throw new Error();
-      })
-      .then((response) => parserRSS(response))
-      .catch(() => {
-        state.feedback = state.i18next.t('loading.errors.errorResource');
-        throw new Error();
-      });
-  });
-
-  Promise.all(promises)
-    .then(({ topics }) => {
-      const newTopics = differenceWith(state.topics, topics, isEqual);
-      if (isEmpty(newTopics)) return;
-      // console.log(...newTopics);
-      watcherUpdateRss.topics.push(...newTopics);
+    // eslint-disable-next-line arrow-body-style
+    const promises = state.resources.map((resource) => {
+      return axios.get(`${proxy}${resource}`)
+        .catch(() => {
+          state.feedback = state.i18next.t('loading.errors.errorNetWork');
+          throw new Error();
+        })
+        .then((response) => parserRSS(response))
+        .catch(() => {
+          state.feedback = state.i18next.t('loading.errors.errorResource');
+          throw new Error();
+        });
     });
-  // .then(() => {
-  //   setTimeout(updateRss(watcherUpdateRss, state), 5000);
-  // });
+
+    Promise.all(promises)
+      .then((parsedRss) => {
+        parsedRss.forEach(({ topics }) => {
+          const newTopics = differenceWith(state.topics, topics, isEqual);
+          if (isEmpty(newTopics)) return;
+          console.log(newTopics);
+          watcherUpdateRss.topics.push(...newTopics);
+        });
+      })
+      .then(() => {
+        updateRss(watcherUpdateRss, state);
+      });
+  }, 5000);
 };
 
 export default updateRss;
