c2c41972d6051473365811c2a83e74f2f3377f17
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-10-25 20:36:27 +0300

    import new rss renders

--- src/view/watchers.js
+++ src/view/watchers.js
@@ -1,6 +1,6 @@
 import onChange from 'on-change';
 import renderFeedback from '../renders/renderFeedback.js';
-import renderRssContent from '../renders/renderRssContent.js';
+import { renderFeeds, renderPosts } from '../renders/renderRssContent.js';
 
 export const watcherFeedback = (state) => {
   const watcher = onChange(state, (path, value) => {
@@ -15,7 +15,10 @@ export const watcherFeedback = (state) => {
 export const watcherLoadingRss = (state) => {
   const watcher = onChange(state, (path, value) => {
     if (value === null) return;
-    if (value) renderRssContent(state);
+    if (value) {
+      renderFeeds(state);
+      renderPosts(state.rssContent.topics, state);
+    }
     renderFeedback(value, state.feedback);
     watcher[path] = null;
   });
