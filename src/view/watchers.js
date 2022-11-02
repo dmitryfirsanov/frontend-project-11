2cdc96191657fbb43d3cefe7f11e8ee20f6b8680
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-11-02 19:41:10 +0300

    add watchers for modal and activity buttons

--- src/view/watchers.js
+++ src/view/watchers.js
@@ -1,6 +1,9 @@
 import onChange from 'on-change';
 import renderFeedback from '../renders/renderFeedback.js';
 import { renderFeeds, renderPosts } from '../renders/renderRssContent.js';
+import renderModalWindow from '../renders/renderModalWindow.js';
+import renderOfReadPosts from '../renders/renderOfReadPosts.js';
+import { lockForm, unlockForm } from '../renders/renderBlockForm.js';
 
 export const watcherFeedback = (state) => {
   const watcher = onChange(state, (path, value) => {
@@ -17,10 +20,45 @@ export const watcherLoadingRss = (state) => {
     if (value === null) return;
     if (value) {
       renderFeeds(state);
-      renderPosts(state.rssContent.topics, state);
+      renderPosts(state.rssContent.posts, state);
     }
     renderFeedback(value, state.feedback);
     watcher[path] = null;
+  });
+
+  return watcher;
+};
+
+export const watcherModalWindow = (state) => {
+  const watcher = onChange(state, (path, value) => {
+    switch (path) {
+      case 'viewedPost':
+        renderModalWindow(value);
+        break;
+      case 'isRead': {
+        renderOfReadPosts(value);
+        break;
+      }
+      default:
+        throw new Error();
+    }
+  });
+
+  return watcher;
+};
+
+export const watcherActivityButton = (state) => {
+  const watcher = onChange(state, (path, value) => {
+    switch (value) {
+      case true:
+        lockForm();
+        break;
+      case false:
+        unlockForm();
+        break;
+      default:
+        throw new Error();
+    }
   });
 
   return watcher;
