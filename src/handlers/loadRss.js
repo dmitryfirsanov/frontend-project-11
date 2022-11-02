a3af5cfc1e781cb65194df0604c4b735381086e7
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-11-02 19:39:40 +0300

    add handler for modal window

--- src/handlers/loadRss.js
+++ src/handlers/loadRss.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-param-reassign */
 import axios from 'axios';
 import parserRss from '../parsers/parserRss';
-import { watcherLoadingRss } from '../view/watchers.js';
-import getIdForTopics from './getIdForTopics';
+import { watcherLoadingRss, watcherActivityButton } from '../view/watchers.js';
+import getIdForPosts from './getIdForPosts';
+import { handlerOfLinkOpeningBtn, handlerOfOpenModalWindow } from './modalWindow.js';
+
 import updateRss from './updateRss';
 
 const loadRss = (url, state) => {
@@ -12,21 +14,27 @@ const loadRss = (url, state) => {
     .catch(() => {
       state.feedback = state.i18next.t('loading.errors.errorNetWork');
       watcherLoadingRss(state).isLoaded = false;
+      watcherActivityButton(state).lock = false;
       throw new Error();
     })
     .then((response) => parserRss(response))
     .catch(() => {
       state.feedback = state.i18next.t('loading.errors.errorResource');
       watcherLoadingRss(state).isLoaded = false;
+      watcherActivityButton(state).lock = false;
       throw new Error();
     })
-    .then(({ feed, topics }) => {
+    .then(({ feed, posts }) => {
       state.rssContent.resources.unshift(url);
       state.rssContent.feeds.unshift(feed);
-      state.rssContent.topics.unshift(...topics);
-      getIdForTopics(state.rssContent.topics);
+      state.rssContent.posts.unshift(...posts);
+      getIdForPosts(state.rssContent.posts);
+
       state.feedback = state.i18next.t('loading.isLoaded');
       watcherLoadingRss(state).isLoaded = true;
+      watcherActivityButton(state).lock = false;
+      handlerOfLinkOpeningBtn(state);
+      handlerOfOpenModalWindow(state);
     })
     .then(() => {
       updateRss(state);
