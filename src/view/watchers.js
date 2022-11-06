f3efdaf587a89c729f46ee6c1e8b827175fc0f0d
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-11-06 17:50:56 +0300

    rewrite watcher for all events

--- src/view/watchers.js
+++ src/view/watchers.js
@@ -5,33 +5,45 @@ import renderModalWindow from '../renders/renderModalWindow.js';
 import renderOfReadPosts from '../renders/renderOfReadPosts.js';
 import { lockForm, unlockForm } from '../renders/renderBlockForm.js';
 
-export const watcherFeedback = (state) => {
-  const watcher = onChange(state, (path, value) => {
-    if (value === null) return;
-    renderFeedback(value, state.feedback);
-    watcher[path] = null;
-  });
-
-  return watcher;
-};
-
-export const watcherLoadingRss = (state) => {
-  const watcher = onChange(state, (path, value) => {
-    if (value === null) return;
-    if (value) {
-      renderFeeds(state);
-      renderPosts(state.rssContent.posts, state);
-    }
-    renderFeedback(value, state.feedback);
-    watcher[path] = null;
-  });
-
-  return watcher;
-};
-
-export const watcherModalWindow = (state) => {
+export default (state, i18n) => {
   const watcher = onChange(state, (path, value) => {
     switch (path) {
+      case 'isValid':
+        renderFeedback(value, state.feedback, i18n);
+        break;
+      case 'loading':
+        switch (value) {
+          case 'sending':
+            lockForm();
+            break;
+          case 'finished':
+            renderFeeds(state, i18n);
+            renderPosts(state, i18n);
+            renderFeedback(true, state.feedback, i18n);
+            unlockForm();
+            break;
+          case 'failed':
+            renderFeedback(false, state.feedback, i18n);
+            unlockForm();
+            break;
+          default:
+            throw new Error();
+        }
+        break;
+      case 'updating':
+        switch (value) {
+          case 'requested':
+            break;
+          case 'updated':
+            renderPosts(state, i18n);
+            break;
+          case 'failed':
+            renderFeedback(false, state.feedback, i18n);
+            break;
+          default:
+            throw new Error();
+        }
+        break;
       case 'viewedPost':
         renderModalWindow(value);
         break;
@@ -39,23 +51,6 @@ export const watcherModalWindow = (state) => {
         renderOfReadPosts(value);
         break;
       }
-      default:
-        throw new Error();
-    }
-  });
-
-  return watcher;
-};
-
-export const watcherActivityButton = (state) => {
-  const watcher = onChange(state, (path, value) => {
-    switch (value) {
-      case true:
-        lockForm();
-        break;
-      case false:
-        unlockForm();
-        break;
       default:
         throw new Error();
     }
