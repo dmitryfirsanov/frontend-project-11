3ffb034d4995c5b8c2087ef01c09fd9d6823cc3a
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-11-06 17:55:56 +0300

    add application initialization

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
-import addRss from './handlers/addRss.js';
+import './styles.scss';
+import 'bootstrap';
+import i18next from 'i18next';
+import languages from './locales/languages.js';
+import addRss from './handlers/addRss';
+import updateRss from './handlers/updateRss';
 
-const app = (state) => {
-  addRss(state);
+const i18n = i18next.createInstance();
+i18n.init({
+  lng: 'ru',
+  debug: true,
+  resources: languages.ru,
+});
+
+const state = {
+  feedback: '',
+  isValid: null,
+  rssContent: {
+    loading: null,
+    updating: null,
+    resources: [],
+    feeds: [],
+    topics: [],
+  },
+  uiState: {
+    viewedPost: {},
+    isRead: [],
+  },
 };
 
-export default app;
+export default () => {
+  addRss(state, i18n);
+  updateRss(state, i18n);
+};
