d29917936c98bb02545f65df8619db47b5406e04
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-11-06 17:53:03 +0300

    get rid of multiple call, add watcher

--- src/handlers/updateRss.js
+++ src/handlers/updateRss.js
@@ -2,58 +2,54 @@
 import axios from 'axios';
 import { differenceWith, isEqual, isEmpty } from 'lodash';
 import parserRSS from '../parsers/parserRss.js';
-import getIdForPosts from './getIdForPosts.js';
-import { renderPosts } from '../renders/renderRssContent.js';
+import watcher from '../view/watchers.js';
+import getIdForTopics from './getIdForTopics.js';
 import { handlerOfLinkOpeningBtn, handlerOfOpenModalWindow } from './modalWindow.js';
 
-const updateRss = (state) => {
-  setTimeout(() => {
-    const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
+const updateRss = (state, i18n) => {
+  const proxy = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
 
-    // eslint-disable-next-line arrow-body-style
-    const promises = state.rssContent.resources.map((resource) => {
-      return axios.get(`${proxy}${resource}`)
-        .then((response) => parserRSS(response))
-        .catch((error) => {
-          switch (error.message) {
-            case 'Network Error':
-              state.feedback = state.i18next.t('loading.errors.errorNetWork');
-              break;
-            case 'Parsing Error':
-              state.feedback = state.i18next.t('loading.errors.errorResource');
-              break;
-            default:
-              throw new Error();
-          }
-          throw new Error();
-        });
+  watcher(state, i18n).updating = 'requested';
+  // eslint-disable-next-line arrow-body-style
+  const promises = state.rssContent.resources.map((resource) => {
+    return axios.get(`${proxy}${resource}`)
+      .then((response) => parserRSS(response))
+      .catch((error) => {
+        switch (error.message) {
+          case 'Network Error':
+            state.feedback = 'update.errors.errorNetWork';
+            break;
+          case 'Parsing Error':
+            state.feedback = 'update.errors.errorResource';
+            break;
+          default:
+            throw new Error();
+        }
+        watcher(state, i18n).updating = 'failed';
+        throw new Error();
+      });
+  });
+
+  Promise.all(promises)
+    .then((parsedRss) => {
+      parsedRss.forEach(({ topics }) => {
+        const oldTopicsLinks = state.rssContent.topics.map((topic) => topic.link);
+        const allTopicsLinks = topics.map((topic) => topic.link);
+        const newTopicsLinks = differenceWith(allTopicsLinks, oldTopicsLinks, isEqual);
+        if (isEmpty(newTopicsLinks)) return;
+
+        const newTopics = newTopicsLinks
+          .map((link) => topics.find((topic) => topic.link === link));
+
+        state.rssContent.topics.unshift(...newTopics);
+        getIdForTopics(state.rssContent.topics);
+        watcher(state, i18n).updating = 'updated';
+        handlerOfLinkOpeningBtn(state, i18n);
+        handlerOfOpenModalWindow(state, i18n);
+      });
     });
 
-    Promise.all(promises)
-      .then((parsedRss) => {
-        parsedRss.forEach(({ posts }) => {
-          const oldPostsLinks = state.rssContent.posts.map((post) => post.link);
-          const allPostsLinks = posts.map((post) => post.link);
-          const newPostsLinks = differenceWith(allPostsLinks, oldPostsLinks, isEqual);
-          if (isEmpty(newPostsLinks)) return;
-
-          const newPosts = newPostsLinks
-            .map((link) => posts.find((post) => post.link === link));
-
-          state.rssContent.posts.unshift(...newPosts);
-          getIdForPosts(state.rssContent.posts);
-          renderPosts(state.rssContent.posts, state);
-          handlerOfLinkOpeningBtn(state);
-          handlerOfOpenModalWindow(state);
-        });
-      })
-      .then(() => {
-        updateRss(state);
-      })
-      .catch(() => {
-        updateRss(state);
-      });
-  }, 5000);
+  setTimeout(() => updateRss(state, i18n), 5000);
 };
 
 export default updateRss;
