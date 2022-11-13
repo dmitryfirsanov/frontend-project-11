e94c7b6cb8bb3b662d29ffbb92f9511888c64f8e
dmitryfirsanov <dmitryfirsanov03@gmail.com>
2022-11-13 19:48:37 +0300

    leave 1 onChange handler

--- src/view.js
+++ src/view.js
@@ -4,54 +4,50 @@ import {
   renderModalWindow, renderLockForm, renderUnlockForm,
 } from './render.js';
 
-export default (state, i18n) => {
-  const watcher = onChange(state, (path, value) => {
-    switch (path) {
-      case 'feedback':
-        renderFeedback(state.isError, value, i18n);
-        break;
-      case 'loading':
-        switch (value) {
-          case 'sending':
-            renderLockForm();
-            break;
-          case 'finished':
-            renderFeeds(state, i18n);
-            renderPosts(state, i18n);
-            renderUnlockForm();
-            break;
-          case 'failed':
-            renderUnlockForm();
-            break;
-          default:
-            throw new Error();
-        }
-        break;
-      case 'updating':
-        switch (value) {
-          case 'requested':
-            break;
-          case 'updated':
-            renderPosts(state, i18n);
-            break;
-          case 'failed':
-            renderFeedback(false, state.feedback, i18n);
-            break;
-          default:
-            throw new Error();
-        }
-        break;
-      case 'viewedPost':
-        renderModalWindow(value);
-        break;
-      case 'isRead': {
-        renderOfReadPosts(value);
-        break;
+export default (state, i18n) => onChange(state, (path, value) => {
+  switch (path) {
+    case 'feedback':
+      renderFeedback(state.isError, value, i18n);
+      break;
+    case 'loading':
+      switch (value) {
+        case 'sending':
+          renderLockForm();
+          break;
+        case 'finished':
+          renderFeeds(state, i18n);
+          renderPosts(state, i18n);
+          renderUnlockForm();
+          break;
+        case 'failed':
+          renderUnlockForm();
+          break;
+        default:
+          throw new Error();
       }
-      default:
-        throw new Error();
+      break;
+    case 'updating':
+      switch (value) {
+        case 'requested':
+          break;
+        case 'updated':
+          renderPosts(state, i18n);
+          break;
+        case 'failed':
+          renderFeedback(false, state.feedback, i18n);
+          break;
+        default:
+          throw new Error();
+      }
+      break;
+    case 'viewedPost':
+      renderModalWindow(value);
+      break;
+    case 'isRead': {
+      renderOfReadPosts(value);
+      break;
     }
-  });
-
-  return watcher;
-};
+    default:
+      throw new Error();
+  }
+});
